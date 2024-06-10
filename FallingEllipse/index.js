13e3647187df2b61ac0a8a1daaa4f86f867e5a05
VaclavVrba <101871159+VaclavVrba@users.noreply.github.com>
2024-06-10 21:20:30 +0200

    Update index.js

--- FallingEllipse/index.js
+++ FallingEllipse/index.js
@@ -1,7 +1,7 @@
 let bubles = [];
 
 function setup(){
-    createCanvas(displayWidth, displayHeight/2);
+    createCanvas(displayWidth, displayHeight/1.53);
 }
 
 function draw() {
@@ -32,9 +32,9 @@ function mousePressed(){
             this.speed = this.speed + this.gravity;
             this.y = this.y + this.speed;
 
-            if(this.y >= displayHeight){
+            if(this.y >= displayHeight/1.53){
                 this.speed = this.speed * (-this.bounce);
-                this.y = displayHeight;
+                this.y = displayHeight/1.53;
             }
 
             else if(this.y <= this.positionY){
